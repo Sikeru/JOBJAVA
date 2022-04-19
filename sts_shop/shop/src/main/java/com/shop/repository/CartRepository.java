ac78e5c0c1f7e510a54dd561ef067a7511722cc1
Sikeru <56014398+Sikeru@users.noreply.github.com>
2022-04-19 14:54:10 +0900

    Delete sts_shop directory

--- sts_shop/shop/src/main/java/com/shop/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.shop.entity.Cart;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-	
-}
--- sts_shop/shop/src/main/java/com/shop/repository/OrderItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.shop.entity.OrderItem;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
-
-}
