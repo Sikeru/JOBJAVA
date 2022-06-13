3ecfb24a70aceaebf87e755e597acaff612a131c
nakiwon <94960615+nakiwon@users.noreply.github.com>
2022-06-13 15:07:14 +0900

    Delete BoardService.java

--- jobJava_0.1/src/main/java/com/jobjava/JJ/counselor/service/BoardService.java
+++
@@ -1,9 +0,0 @@
-package com.jobjava.JJ.counselor.service;
-
-import java.util.List;
-
-import com.jobjava.JJ.counselor.vo.ArticleVO;
-
-public interface BoardService {
-	public List<ArticleVO> listArticles() throws Exception;
-}
